1c3aed7f6cbffbc47aa6ab296ac56f17ed45acbc
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-07-03 11:35:03 +0100

    chore: d.ts with tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
     clean: true,
-    entry: ["src/index.ts"],
-    format: ["esm"],
+    dts: true,
     sourcemap: true,
     minify: false,
+    entry: ["src/index.ts"],
+    format: ["esm"],
     target: "esnext",
     outDir: "dist",
 })
