ab61292224e14a04124fc4e6b0db2ff8c31ea16e
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-07-03 09:41:54 +0100

    chore: deriving from only the challenge

--- static/src/ethereum/key-gen/derive.ts
+++ static/src/ethereum/key-gen/derive.ts
@@ -2,11 +2,9 @@ import { keccak256 } from "ethers";
 import { strToUint8Array } from "../../utils/shared";
 
 // Function to derive a deterministic Ethereum private key
-export function deriveEthereumPrivateKey(userId: string, challenge: string): string {
-    const dataToHash = strToUint8Array(userId + challenge);
+export function deriveEthereumPrivateKey(challenge: string): string {
+    const dataToHash = strToUint8Array(challenge);
     const pad32 = new Uint8Array(32);
     dataToHash.set(pad32, dataToHash.length - pad32.length);
-    const pk = keccak256(dataToHash)
-    console.log("Derived Ethereum Private Key:", pk)
-    return pk;
+    return keccak256(dataToHash);
 }
